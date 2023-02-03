f1f617f9de746674888a57bc12bec0ed4b4fefdd
Sara Pesavento <sarapesavento@gmail.com>
2023-02-02 21:10:53 -0800

    season 1 episode 3

---
+++ korean_subtitle_extraction.py
@@ -0,0 +1,12 @@
+import webvtt
+
+vtt_file = "./subtitle_files/Avatar_S01E03.vtt"
+txt_file = "./clean_files/Avatar_S01E03.txt"
+
+with open(txt_file, 'w') as f:
+    for caption in webvtt.read(vtt_file):
+        #print(caption.start)
+        # #print(caption.end)
+        # print(caption.text)
+        f.write(caption.text)
+
