9c79c8c9b07053e03081ac2257f7e8ed45591b7b
Sara Pesavento <sarapesavento@gmail.com>
2023-02-02 21:35:58 -0800

    add new line

--- korean_subtitle_extraction.py
+++ korean_subtitle_extraction.py
@@ -5,8 +5,9 @@ txt_file = "./clean_files/Avatar_S01E03.txt"
 
 with open(txt_file, 'w') as f:
     for caption in webvtt.read(vtt_file):
-        #print(caption.start)
+        # print(caption.start)
         # #print(caption.end)
         # print(caption.text)
+        f.write('\n')
         f.write(caption.text)
 
